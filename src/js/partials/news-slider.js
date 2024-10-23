3ff1ae787e6011fc8c7232ce0ff90138f61beb9a
Yuhlimov Mikhail <mthat@vk.com>
2024-10-23 16:13:58 +0400

    31140 team 980 adaptive

--- src/js/partials/news-slider.js
+++ src/js/partials/news-slider.js
@@ -1,16 +1,32 @@
-$('.b-news-slider').slick({
-    // normal options...
+$('.b-post-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
     infinite: true,
-    // the magic
+    asNavFor: '.b-news-slider'
+});
+
+$('.b-news-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    fade: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    asNavFor: '.b-post-slider',
+    prevArrow: '<button class=\'b-button b-button--left-arrow\' type=\'button\'>',
+    nextArrow: '<button class=\'b-button b-button--right-arrow\' type=\'button\'>',
+
     responsive: [{
+
+        breakpoint: 980,
         settings: {
-            slidesToShow: 1,
-            infinite: true
+            arrows: true,
+            dots: false,
         }
 
-    }],
-
-    prevArrow: '<button class=\'b-button b-button--left-arrow\' type=\'button\'>',
-    nextArrow: '<button class=\'b-button b-button--right-arrow\' type=\'button\'>'
-
+    }]
 });

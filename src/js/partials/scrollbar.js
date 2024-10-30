41b0d0ee71f0c5f97af43e15fb955c3f4bd87bac
Yuhlimov Mikhail <mthat@vk.com>
2024-10-30 10:19:56 +0400

    31140 scrollbar js error fix

--- src/js/partials/scrollbar.js
+++ src/js/partials/scrollbar.js
@@ -1,32 +1,34 @@
-const container = document.querySelector('body');
-const content = document.querySelector('.b-page-wrapper');
-const scrollbar = document.querySelector('.b-custom-scrollbar');
+(function () {
+    const container = document.querySelector('body');
+    const content = document.querySelector('.b-page-wrapper');
+    const scrollbar = document.querySelector('.b-custom-scrollbar');
 
-function updateScrollbar() {
-    const contentRatio = container.clientHeight / content.scrollHeight;
-    scrollbar.style.height = `${contentRatio * 100}%`;
-    const scrollRatio = content.scrollTop / (content.scrollHeight - container.clientHeight);
-    scrollbar.style.top = `${scrollRatio * (container.clientHeight - scrollbar.clientHeight)}px`;
-}
-
-content.addEventListener('scroll', updateScrollbar);
-updateScrollbar();
-
-scrollbar.addEventListener('mousedown', (e) => {
-    const offset = e.clientY - scrollbar.getBoundingClientRect().top;
-
-    function onMouseMove(event) {
-        const scrollTop = event.clientY - container.getBoundingClientRect().top - offset;
-        const scrollPercent = scrollTop / (container.clientHeight - scrollbar.clientHeight);
-        content.scrollTop = scrollPercent * (content.scrollHeight - container.clientHeight);
-        updateScrollbar();
+    function updateScrollbar() {
+        const contentRatio = container.clientHeight / content.scrollHeight;
+        scrollbar.style.height = `${contentRatio * 100}%`;
+        const scrollRatio = content.scrollTop / (content.scrollHeight - container.clientHeight);
+        scrollbar.style.top = `${scrollRatio * (container.clientHeight - scrollbar.clientHeight)}px`;
     }
 
-    function onMouseUp() {
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-    }
+    content.addEventListener('scroll', updateScrollbar);
+    updateScrollbar();
 
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-});
+    scrollbar.addEventListener('mousedown', (e) => {
+        const offset = e.clientY - scrollbar.getBoundingClientRect().top;
+
+        function onMouseMove(event) {
+            const scrollTop = event.clientY - container.getBoundingClientRect().top - offset;
+            const scrollPercent = scrollTop / (container.clientHeight - scrollbar.clientHeight);
+            content.scrollTop = scrollPercent * (content.scrollHeight - container.clientHeight);
+            updateScrollbar();
+        }
+
+        function onMouseUp() {
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseup', onMouseUp);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+    });
+})();
